407ae7ba23af7baf332267a1912dea767e7d248e
hollopte <hollopte@msu.edu>
2016-09-19 20:36:06 -0400

    Buttons link to new blank activity called display_products

--- app/src/main/java/com/example/cse498/testapplication/MainActivity.java
+++ app/src/main/java/com/example/cse498/testapplication/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.cse498.testapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    //initialize button
+    Button btnCredit = (Button) findViewById(R.id.creditBtn);
+
+   //btnCredit.setOnClickListener(new View.OnContextClickListener());
+}
+    public void OnClick(View v) {
+        Intent intent = new Intent(MainActivity.this, display_products.class);
+        startActivity(intent);
     }
+
+
 }
