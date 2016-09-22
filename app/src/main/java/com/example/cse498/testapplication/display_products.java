68758796dd60c61850f56e80c7d4ff47dbc83662
hollopte <hollopte@msu.edu>
2016-09-22 19:38:28 -0400

    Merge files, Intent in display set up, stars added in layout of display products

--- app/src/main/java/com/example/cse498/testapplication/display_products.java
+++ app/src/main/java/com/example/cse498/testapplication/display_products.java
@@ -2,7 +2,8 @@ package com.example.cse498.testapplication;
 
 import android.app.Activity;
         import android.app.ListActivity;
-        import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
         import android.view.View;
         import android.widget.AdapterView;
         import android.widget.AdapterView.OnItemClickListener;
@@ -23,6 +24,7 @@ public class display_products extends Activity{
             R.mipmap.creditcards,
             R.mipmap.creditcards2
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,10 +39,15 @@ public class display_products extends Activity{
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                String Slecteditem= itemname[+position];
-                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
-
+                //String Slecteditem= itemname[+position];
+                //Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+                //create an even to link the first activity to display products on the second
+                Intent click_intent = new Intent(display_products.this, MainActivity.class);
+                startActivity(click_intent);
             }
         });
     }
+
+
+
 }
