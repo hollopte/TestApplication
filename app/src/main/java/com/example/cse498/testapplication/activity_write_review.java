4268100edd9e093355e929c8727e45b995bd877c
frisbiem <frisbiem@msu.edu>
2016-09-22 20:12:53 -0400

    Created new activity write review creating java class with it. Deleted files not used.

---
+++ app/src/main/java/com/example/cse498/testapplication/activity_write_review.java
@@ -0,0 +1,13 @@
+package com.example.cse498.testapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_write_review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_review);
+    }
+}
