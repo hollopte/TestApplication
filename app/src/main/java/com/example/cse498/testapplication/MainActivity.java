39d50800debdef4282792553defaf9f0653c9a33
hollopte <hollopte@msu.edu>
2016-09-23 20:31:05 -0400

    Menu Bar in Nav added

--- app/src/main/java/com/example/cse498/testapplication/MainActivity.java
+++ app/src/main/java/com/example/cse498/testapplication/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.cse498.testapplication;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -21,6 +26,42 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //something to do with back arrow in navigation bar
+        //getSupportActionBar().setHomeButtonEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_options_menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemsSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.home){
+
+            MainActivity.this.finish();
+            return true;
+        } else if (id == R.id.ViewAllProducts){
+            //Toast.makeText(getApplicationContext(), "Settings option hit!", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.ViewCategories){
+            //Toast.makeText(getApplicationContext(), "item1 option hit!", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.Prefrences){
+            //Toast.makeText(getApplicationContext(), "item2 otion hit!", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.Logout){
+            //Toast.makeText(getApplicationContext(), "item3 option hit!", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
     }
 
 
